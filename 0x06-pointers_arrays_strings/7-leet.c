d00f7dcf48ec563cfd696723614e7ca87902d594
moanbekele <moanbekele@gmail.com>
2022-10-28 07:53:30 -0700

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,28 @@
 #include "main.h"
-/**
- * leet - encodes a string
- * @str: the string to encode
- *
- * Return: the encode string
- */
-char *leet(char *str)
-{
-int i, j;
-char c[] = "aAeEoOtTlL";
-char n[] = "4433007711";
 
-i = 0;
-while (str[i] != '\0')
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *s)
 {
-j = 0;
-while (c[j] != '\0')
-{
-if (str[i] == c[j])
-{
-str[i] = n[j];
-}
-j++;
-}
-i++;
-}
-return (str);
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
 }
